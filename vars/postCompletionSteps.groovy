074f6e2c3bcfc6bddb2957fd824d29a3f47001aa
arprasto <arpit11rastogi@gmail.com>
2021-04-21 22:37:30 +0530

    test

--- vars/postCompletionSteps.groovy
+++ vars/postCompletionSteps.groovy
@@ -2,16 +2,16 @@ def call(buildResult){
 
     def util = new commonUtil()
     def notifyUtil = new notifyUtility()
-   
-    if( buildResult == "SUCCESS" ) {
-        
-        util.tagOnComplete()
-        
-    }
-    
-    notifyUtil.notify(buildResult)	
-       
 
-    
-         
+    if( buildResult == "SUCCESS" ) {
+
+        util.tagOnComplete()
+        echo "${buildResult}"
+    }
+
+    notifyUtil.notify(buildResult)
+
+
+
+
 }
