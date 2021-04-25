7d5a13023f525e030012c241bbd230c0711fe1f9
arprasto <arpit11rastogi@gmail.com>
2021-04-25 18:07:43 +0530

    test

--- vars/createDir.groovy
+++ vars/createDir.groovy
@@ -2,9 +2,8 @@ import java.io.File
 
 def call(CHECKOUT_PATH){
 def File f = new File(CHECKOUT_PATH);
-echo "is folder exists ${f.isDirectory()}"
-if ("${f.isDirectory()}".matches("True")){
-echo "folder already exists ${CHECKOUT_PATH}"
+if ("${f.isDirectory()}".matches("true")){
+echo "folder already exists ${CHECKOUT_PATH} skipping create dir"
 }
 else{
 echo "creating checkout dir ${CHECKOUT_PATH}"
