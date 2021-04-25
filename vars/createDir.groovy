95e20f0b5d699e9205aa255ec4bca0b077da92b9
arprasto <arpit11rastogi@gmail.com>
2021-04-25 16:27:55 +0530

    test

--- vars/createDir.groovy
+++ vars/createDir.groovy
@@ -1,6 +1,5 @@
 def call(CHECKOUT_PATH){
 echo "creating checkout dir ${CHECKOUT_PATH}"
-sh """ chmod 777 ${CHECKOUT_PATH}/.. """
 sh """ mkdir ${CHECKOUT_PATH} """
 echo "setting permissions for ${CHECKOUT_PATH}"
 sh """ chmod 777 ${CHECKOUT_PATH} """
