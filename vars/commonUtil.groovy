79abcd0b9e46a2665aed4378e040f50fbeccd491
arprasto <arpit11rastogi@gmail.com>
2021-04-25 22:11:50 +0530

    test

--- vars/commonUtil.groovy
+++ vars/commonUtil.groovy
@@ -207,11 +207,11 @@ def updateBootstrapProperties(PROPERTY,VALUE){
                 String splitKey = key.substring(PROPERTY.length())
                 String value = properties.getProperty(key)
                 //echo "setting ${key} with value ${VALUE}"
-                properties.setProperty(key, ${VALUE})
+                properties.setProperty(key, VALUE)
             }else{
                 String value = properties.getProperty(key)
                 //echo "setting ${key} with value ${value}"
-                properties.setProperty(PROPERTY, ${value})
+                properties.setProperty(PROPERTY, value)
             }
         }
         if(!propertyfound)
