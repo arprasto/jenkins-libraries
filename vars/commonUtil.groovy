989b7c9c2c71246b8f37d7fdb97ac6ad5c6c06fa
arprasto <arpit11rastogi@gmail.com>
2021-05-10 19:13:24 +0530

    test

--- vars/commonUtil.groovy
+++ vars/commonUtil.groovy
@@ -120,19 +120,18 @@ def SetEnvironment()
         if [ -d "${env.JENKINS_HOME}/ant_home/apache-ant-1.10.6/bin" ]; then
           echo "ant installation already exists"
           export ANT_HOME="${env.JENKINS_HOME}/ant_home/apache-ant-1.10.6"
-          export PATH="${env.PATH}:$ANT_HOME/bin"
         else
             wget https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.6-bin.zip
             unzip -x apache-ant-1.10.6-bin.zip
             if [ -z ${env.ANT_HOME} ]; then
                 echo "setting ANT_HOME"
                 export ANT_HOME="${env.JENKINS_HOME}/ant_home/apache-ant-1.10.6"
-                export PATH="${env.PATH}:${env.ANT_HOME}/bin"
             else
                 echo "ANT_HOME already setup to ${env.ANT_HOME}"
             fi
         fi
     """
+    env.PATH="${env.PATH}:${env.ANT_HOME}/bin"
     }
 }
 
